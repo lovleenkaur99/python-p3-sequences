6d1e69274ed1921c91234748663bbb30533c3bf5
lovleenkaur <lovleenk99@gmail.com>
2023-08-23 17:51:31 -0400

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    fib_seq = []
+    if length > 0:
+        fib_seq.append(0)   
+    if length >= 2:
+        fib_seq.append(1)
+        for i in range(2, length):
+            fib_seq.append(fib_seq[-1] + fib_seq[-2])
+
+    print(fib_seq)
+    # fibonacci = []
+    # if length > 0:
+    #     fibonacci.append(0)
+    # if length >= 2:
+    #     fibonacci.append(1)
